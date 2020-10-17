bc272805ef1f72f601b602bc9be501ae69c9d332
Matheus <matheusfrancoalves@hotmail.com>
2020-10-17 18:04:22 -0300

    separate TopContentDesktop into a component

---
+++ src/components/TopContentDesktop/styles.ts
@@ -0,0 +1,96 @@
+import styled from 'styled-components';
+
+export const Content = styled.div`
+  width: 1120px;
+
+  margin: 0 auto;
+`;
+
+export const TopContainer = styled.div`
+  width: 100%;
+  height: 224px;
+`;
+
+export const AvatarAndTechsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Avatar = styled.img`
+  height: 200px;
+  width: 200px;
+  border-radius: 8px;
+`;
+
+export const TechsContainer = styled.div`
+  height: 200px;
+  width: 700px;
+  border-radius: 8px;
+  background: #2c2c2c;
+  border: 1px solid #0179cb;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 48px;
+
+  svg {
+    border-radius: 60px;
+  }
+
+  .react {
+    color: #48cef7;
+  }
+
+  .ts {
+    color: #027fca;
+  }
+
+  .js {
+    color: #efd81d;
+  }
+
+  .css {
+    color: #2196f3;
+  }
+
+  .github {
+  }
+
+  .vscode {
+    color: #24a4eb;
+  }
+`;
+
+export const DescriptionAndContact = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  color: #e9f6ff;
+`;
+
+export const Description = styled.div`
+  width: 200px;
+
+  display: flex;
+  align-items: center;
+
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+export const Contact = styled.div`
+  width: 700px;
+  padding: 16px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  a {
+    color: #e9f6ff;
+    text-decoration: none;
+    margin: 0 24px;
+    font-size: 18px;
+    font-weight: 600;
+  }
+`;
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,71 +1,15 @@
 import React from 'react';
-import { FaReact } from 'react-icons/fa';
-import {
-  SiTypescript,
-  SiJavascript,
-  SiCss3,
-  SiGithub,
-  SiVisualstudio,
-} from 'react-icons/si';
-
-import ImageAvatar from '../../assets/perfil.png';
 
 import DesktopHeader from '../../components/DesktopHeader';
+import TopContent from '../../components/TopContentDesktop';
 
-import {
-  Container,
-  Content,
-  TopContainer,
-  AvatarAndTechsContainer,
-  Avatar,
-  TechsContainer,
-  DescriptionAndContact,
-  Description,
-  Contact,
-} from './styles';
+import { Container } from './styles';
 
 const Main: React.FC = () => {
   return (
     <Container>
       <DesktopHeader />
-
-      <Content>
-        <TopContainer>
-          <AvatarAndTechsContainer>
-            <Avatar src={ImageAvatar} />
-
-            <TechsContainer>
-              <FaReact size={56} className="react" />
-              <SiTypescript size={56} className="ts" />
-              <SiJavascript size={56} className="js" />
-              <SiCss3 size={56} className="css" />
-              <SiGithub size={56} className="github" />
-              <SiVisualstudio size={56} className="vscode" />
-            </TechsContainer>
-          </AvatarAndTechsContainer>
-
-          <DescriptionAndContact>
-            <Description>Front-end Developer</Description>
-
-            <Contact>
-              <a
-                href="https://github.com/Matheus-Franco"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Github
-              </a>
-              <a
-                href="https://www.linkedin.com/in/matheus-franco-713b0b1a3/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Linkedin
-              </a>
-            </Contact>
-          </DescriptionAndContact>
-        </TopContainer>
-      </Content>
+      <TopContent />
     </Container>
   );
 };
